b9fd72b32d62675eeaff4b6ed52b194090364315
Rachel Simm <rachel.simm@domesticandgeneral.com>
2024-01-24 15:11:16 +0000

    create a new folder

--- app/rachel/page.tsx
+++ app/rachel/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import DgInteraction from "@/react-email-starter/emails/dg-interaction";
+import DgInteraction2 from "@/react-email-starter/emails/dg-interaction-2";
 import { render } from "@react-email/render";
 
 import { sendMail } from "@/app/mailMethods/sendMail";
@@ -9,8 +9,15 @@ import { sendMail } from "@/app/mailMethods/sendMail";
 
 export default function Home() {
   const renderEmail = () => {
+    const customerDetails = {
+      title: "Mrs",
+      surname: "Smith",
+      brand: "Bosch",
+      appliance: "Washing Dryer",
+      periodOfCover: 12,
+    };
 
-    const htmlMail = render(DgInteraction());
+    const htmlMail = render(DgInteraction2(customerDetails));
     console.log(htmlMail);
 
     return htmlMail;
